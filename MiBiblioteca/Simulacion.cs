52a8f311777328a273bbd4eff4a1557db85fffe4
LunaEnzo875 <enzoluna760@gmail.com>
2026-05-06 17:29:10 -0300

    Mejoras

--- MiBiblioteca/Simulacion.cs
+++ MiBiblioteca/Simulacion.cs
@@ -4,8 +4,20 @@ namespace SimBolillero;
 
 public class Simulacion
 {
-    public long simularSinHilos(Bolilerro Bolillero,)
+    public long simularSinHilos(Bolillero bolillero, List<int> jugada, int CantidadSimulacion)
     {
-        
+        long Ganadas = 0;
+
+        for (int i = 0; i < CantidadSimulacion; i++)
+        {
+            if (bolillero.jugada(jugada))
+            {
+                Ganadas++;
+            }
+        }
+
+        return Ganadas;
     }
+        
+
 }
