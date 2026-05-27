4a9357e2c65582308bd000ef11dee787e936e694
LunaEnzo875 <enzoluna760@gmail.com>
2026-05-27 18:19:45 -0300

    mejoras

--- MiBiblioteca/simulacion.cs
+++ MiBiblioteca/simulacion.cs
@@ -22,25 +22,34 @@ public class Simulacion
         public long SimularConHilos(Bolillero bolillero, List<int> jugada, int CantidadSimulacion, int cantidadHilos)
         {
             var tareas = new List<Task<long>>();
-
+            long Ganadas = 0;
             int baseCantidad = CantidadSimulacion / cantidadHilos;
             int resto = CantidadSimulacion % cantidadHilos;
-
+            
             for (int i = 0; i < cantidadHilos; i++)
             {
                 int cantidadParaEsteHilo = baseCantidad + (i < resto ? 1 : 0);
 
                 tareas.Add(Task.Run(() =>
-                    simularSinHilos(bolillero.ClonDeLaListaBolillero(), jugada, cantidadParaEsteHilo)
+
+             for (int i = 0; i < CantidadSimulacion; i++);
+            
+                if (bolillero.jugada(jugada))
+                {
+                    Ganadas++;
+                }
+
+            return Ganadas
                 ));
             }
 
             var resultados = Task.WhenAll(tareas).Result;
             return resultados.Sum();
         }
+        
     public async long SimularConHilosAsync(Bolillero bolillero, List<int> jugada, int CantidadSimulacion, int cantidadHilos)
     {
-        var tareas = new List<Task<long>>();
+            var tareas = new List<Task<long>>();
 
             int baseCantidad = CantidadSimulacion / cantidadHilos;
             int resto = CantidadSimulacion % cantidadHilos;
